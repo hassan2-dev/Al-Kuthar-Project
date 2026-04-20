0cb2907437b30559fc9974d5668998776c07ab8e
zahraa-jasim <zahraa.jb25@gmail.com>
2026-04-20 12:10:07 +0300

    Update Open Graph and Twitter meta tags in index.html and configure dynamic URL injection in vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const siteUrl = (env.VITE_SITE_URL || 'https://www.al-kuthar.com').replace(/\/$/, '')
+  const ogImageUrl = `${siteUrl}/logo.png`
+
+  return {
+    plugins: [
+      react(),
+      {
+        name: 'html-inject-og-urls',
+        transformIndexHtml(html) {
+          return html
+            .replace(/__OG_SITE_URL__/g, siteUrl)
+            .replace(/__OG_IMAGE_URL__/g, ogImageUrl)
+        },
+      },
+    ],
+  }
 })
